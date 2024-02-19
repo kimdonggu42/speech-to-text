7a7dc56635dc5b3e63dce8954738663ec650ecbe
kimdonggu42 <kdk3280@undermilli.com>
2024-02-19 14:10:49 +0900

    refactor: upload 페이지 리펙토링

--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -1,29 +1,42 @@
 'use client';
 
 import axios from 'axios';
-import { useState } from 'react';
-
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+import { useState, useRef } from 'react';
 
 export default function Upload() {
+  const [audioFile, setAudioFile] = useState<FormData | null>(null);
+  const [audioFilePathName, setAudioFilePathName] = useState<string>('');
   const [text, setText] = useState<string>('');
   const [executionTime, setExecutionTime] = useState<number>(0);
 
-  const uploadFile = async (e: any) => {
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
+
+  const handleUploadButtonClick = () => {
+    fileInputRef.current?.click();
+  };
+
+  const uploadFile = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && fileInputRef.current?.value !== undefined) {
+      const mp3File = e.target.files[0];
+      const formData = new FormData();
+      formData.append('file', mp3File);
+      setAudioFilePathName(fileInputRef.current?.value);
+      setAudioFile(formData);
+    }
+  };
+
+  const translate = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const mp3File = e.target.files[0];
-    const formData = new FormData();
-    formData.append('file', mp3File);
-    console.log(mp3File);
     try {
-      const res = await axios.post(`${BASE_URL}/transcribe/`, formData);
-      console.log(res);
-      const { status, data } = res;
-      if (status === 200) {
-        setText(data.text);
-        setExecutionTime(data.execution_time_seconds);
+      if (audioFile) {
+        const res = await axios.post(`${process.env.NEXT_PUBLIC_BASE_URL}/transcribe/`, audioFile);
+        const { status, data } = res;
+        if (status === 200) {
+          setText(data.text);
+          setExecutionTime(data.execution_time_seconds);
+        }
       } else {
-        throw new Error('something was wrong!');
+        alert('오디오 파일을 업로드해주세요.');
       }
     } catch (err) {
       console.error(err);
@@ -31,14 +44,31 @@ export default function Upload() {
   };
 
   return (
-    <main>
-      <form>
-        <input className='border border-solid border-black' type='file' onChange={uploadFile} />
-      </form>
-      <div>
-        <div>{text}</div>
-        <div>{executionTime}</div>
+    <div className='flex h-screen items-center justify-center'>
+      <div className='flex flex-col gap-y-[50px] text-[15px]'>
+        <form className='flex h-[30px] gap-x-[10px]' onSubmit={translate}>
+          <input
+            className='w-[300px] border border-solid border-black px-[5px]'
+            placeholder={audioFilePathName}
+            disabled
+          />
+          <input className='hidden' type='file' onChange={uploadFile} ref={fileInputRef} />
+          <button
+            className='bg-teal-900 px-[5px] text-white'
+            type='button'
+            onClick={handleUploadButtonClick}
+          >
+            오디오 파일 업로드
+          </button>
+          <button className='bg-[#2DD4BF] px-[5px] text-white' type='submit'>
+            텍스트 추출
+          </button>
+        </form>
+        <div className='flex flex-col gap-y-[10px]'>
+          <div>추출한 텍스트: {text}</div>
+          <div>실행 시간: {executionTime}</div>
+        </div>
       </div>
-    </main>
+    </div>
   );
 }
