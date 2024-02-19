eb308a013955217d1578e13fab075651986790a5
kimdonggu42 <kdk3280@undermilli.com>
2024-02-19 11:58:11 +0900

    메인 페이지 카테고리 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,27 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
+const category = [{ path: 'upload' }, { path: 'record' }, { path: 'share' }];
+
 export default function Home() {
-  return <main className='text-[30px]'>메인 페이지</main>;
+  const router = useRouter();
+
+  return (
+    <main className='flex h-screen items-center justify-center'>
+      <ul className='flex w-[300px] flex-col gap-y-[20px] text-[30px]'>
+        {category.map((value, index) => {
+          return (
+            <li
+              key={index}
+              className='cursor-pointer bg-teal-900 text-center text-white'
+              onClick={() => router.push(`/${value.path}`)}
+            >
+              {value.path} page
+            </li>
+          );
+        })}
+      </ul>
+    </main>
+  );
 }
